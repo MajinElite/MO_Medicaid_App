da8d479772c7e2427278b94ebeb3584734c2a950
Ammar Osmun <aosmun@my.stlcc.edu>
2026-02-19 17:30:46 -0600

    test

---
+++ Main/Back-end/storage.py
@@ -0,0 +1,30 @@
+"""
+DEVELOPER: Ammar Osmun, Tommy Ngo
+TASK: Sprint 2
+Backend Storage for Employment Verification. Handles saving and retrieving data.
+
+"""
+
+def save_verification(data):
+    """
+    Save verification data somewhere.
+    Options:
+    - JSON file
+    - SQLite database
+    - Simple text file
+
+    Return True if saved.
+    """
+
+    # TODO: Implement saving logic
+    return True
+
+
+def get_all_verifications():
+    """
+    Retrieve all stored verification records.
+    Used later for review/approve page.
+    """
+
+    # TODO: Implement retrieval logic
+    return []
